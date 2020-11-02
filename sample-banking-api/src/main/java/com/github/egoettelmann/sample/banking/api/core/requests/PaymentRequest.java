5814d95f132236574dec99c1764efde603150d0d
Elio Goettelmann <elio.goettelmann@gmail.com>
2020-11-02 23:26:18 +0100

    Added request validation

--- sample-banking-api/src/main/java/com/github/egoettelmann/sample/banking/api/core/requests/PaymentRequest.java
+++ sample-banking-api/src/main/java/com/github/egoettelmann/sample/banking/api/core/requests/PaymentRequest.java
@@ -2,15 +2,20 @@ package com.github.egoettelmann.sample.banking.api.core.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class PaymentRequest {
 
+    @NotNull
     private BigDecimal amount;
     private String currency;
 
+    @NotNull
     private Long giverAccountId;
+
+    @NotNull
     private String beneficiaryAccountNumber;
     private String beneficiaryName;
     private String communication;
